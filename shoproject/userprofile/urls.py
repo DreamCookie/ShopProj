810191ce5605911b396ab781725d52913364ccff
Maxim Makarov <MakMak9595@gmail.com>
2023-03-18 12:29:03 +0300

    Added: 16 from Progress-list.md

--- shoproject/userprofile/urls.py
+++ shoproject/userprofile/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('my_account/', views.my_account, name='my_account'),
+    path('my-store/', views.my_store, name='my_store'),
+    path('my-store/add-product/', views.add_product, name='add_product'),
+    path('my-srote/edit-product/<int:pk>/', views.edit_product, name='edit_product'),
     path('vendor/<int:pk>/', views.vendor_detail, name='vendor_detail'),
 
 ]
